4efa1960658f80f69e9655bd6c141619c62d7c6e
绯一 <jaredlee.ljj@alipay.com>
2016-04-21 14:01:17 +0800

    [feat] refactor calculate path and refine links of context

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.0
+
+`2016-04-21`
+
+- refactor calculate path and refine links of context
--- index.js
+++ index.js
@@ -16,29 +16,29 @@ module.exports = function(content) {
 
   const options = this.options;
   const resourcePath = this.resourcePath;
+  const resource = new util.Resource(options.cwd, options.demoSource, resourcePath);
 
   const query = loaderUtils.parseQuery(this.query);
-
-  const name = path.relative(options.cwd, resourcePath);
 
   const tpl = query.template;
   this.addDependency(tpl);
 
   const scripts = [
-    path.relative(resourcePath, path.join(options.cwd, options.tplSource, 'common.js')),
-    `${path.basename(resourcePath, path.extname(resourcePath))}.js`,
+    path.relative(resourcePath, path.join(resource.demoPath, 'common.js')),
+    `${resource.name}.js`,
   ];
 
   const link = {};
-  link['Index'] = path.relative('../', path.relative(name, './index'));
+  link['Index'] = path.relative('../', path.relative(resource.path, './index'));
   Object.keys(options.entry).forEach(function(key) {
-    link[key] = path.relative('../', path.relative(name, key));
+    link[path.relative(options.demoSource, key)] = path.relative('../', path.relative(resource.path, key));
   });
 
   const html = ejs.render(fs.readFileSync(tpl, 'utf-8'), {
     file: {
       link: link,
-      title: name,
+      title: resource.relativeToCwd + resource.ext,
+      resource: resource,
       script: scripts,
       desc: util.marked([{
         type: 'h2',
